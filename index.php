c34e985ddd8745fad8b97b824141595053753bb5
Joe Kuczek <jkuczek@iastate.edu>
2017-07-27 14:59:43 -0500

    IMPORTANT add the meta viewport

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
         <link rel="stylesheet/less" type="text/less" href="./assets/css/menubar.less" />
